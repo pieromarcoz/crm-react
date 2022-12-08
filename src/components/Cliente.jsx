341fef3fb7a1d8a7ef23950f0f3de1b518774569
piero965d <piero965d@gmail.com>
2022-12-08 03:12:28 -0500

    'useNavigate'

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,21 @@
+const Cliente = ({cliente}) => {
+    const {nombre, empresa, email, telefono, id} = cliente
+  return (
+    <tr className="border-b">
+        <td className='p-6 space-y-2'>
+            <p className="text-2xl text-gray-800">{nombre}</p>
+            <p>{empresa}</p>
+        </td>
+        <td className="p-6">
+            <p className="text-gray-600"><span className="font-bold text-gray-800 uppercase">Email: </span>{email}</p>
+            <p className="text-gray-600"><span className="font-bold text-gray-800 uppercase">Tel: </span>{telefono}</p>
+        </td>
+        <td className="p-6 flex gap-3">
+            <button type="buttom" className="uppercase font-bold text-blue-600 hover:text-blue-700 text-xs">Editar</button>
+            <button type="buttom" className="uppercase font-bold text-red-600 hover:text-red-700 text-xs">Eliminar</button>
+        </td>
+    </tr>
+  )
+}
+
+export default Cliente
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,7 +1,28 @@
-
+import { useNavigate } from 'react-router-dom'
+import Formulario from '../components/Formulario'
 const NuevoCliente = () => {
+  const navigate = useNavigate()
   return (
-    <div>NuevoCliente</div>
+    <>
+      <h1 className="text-4xl text-blue-900 font-black">Nuevo Cliente</h1>
+      <p className="mt-3">Completa todos los campos para registrar un nuevo cliente</p>
+      <div className="flex justify-end">
+        <button 
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase mt-5"
+          onClick={() => navigate('/')}
+        >
+          Volver
+        </button>
+      </div>
+      <div className='bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20'>
+        <Formulario/>
+        <input 
+          type={'submit'} 
+          className='mt-5 w-full bg-blue-800 uppercase p-3 font-bold text-white text-lg ' 
+          value={'Registrar Cliente'}
+        />
+      </div>
+    </>
   )
 }
 
